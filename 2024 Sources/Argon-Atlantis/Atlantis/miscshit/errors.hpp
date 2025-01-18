ad4a5eefaf30f58dc9674a8103b743b055d69ec3
Iris <39096994+RiisDev@users.noreply.github.com>
2025-01-18 16:23:48 -0700

    Delete 2024 Sources/Argon-Atlantis directory

--- 2024 Sources/Argon-Atlantis/Atlantis/miscshit/errors.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include "../Luau/lua.h"
-#include <string>
-
-namespace managers::errors {
-    // adds a cfunction that gets handled by this manager
-    auto add_handled_closure(lua_CFunction cf, std::string_view name) -> void;
-}
